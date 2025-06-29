e492a243da4e37ca65f122ea350a18edc5e7946f
RhPark <cli7942lic@gmail.com>
2025-06-29 20:52:42 +0900

    - Logx 코드 개선

--- app/src/main/java/kr/open/library/logx/MainActivity.kt
+++ app/src/main/java/kr/open/library/logx/MainActivity.kt
@@ -97,40 +97,11 @@ class MainActivity : AppCompatActivity() {
         msg.logxP(tag)
 
 
-        val jsonType = "{'contacts': [{'contact_type': 'individual',\n" +
-                "   'is_green_match': True,\n" +
-                "   'is_signatory': True,\n" +
-                "   'match_score': 101,\n" +
-                "   'persons': [{'addresses': [{'city': 'NEW YORK',\n" +
-                "       'line1': '31 W 34TH ST',\n" +
-                "       'postal_code': '10001',\n" +
-                "       'state_code': 'NY'}],\n" +
-                "     'display': 'Ishay Oved',\n" +
-                "     'emails': [],\n" +
-                "     'first_name': 'Ishay',\n" +
-                "     'id': '8e6d391d-0fa8-5e9d-9880-fd1069c4d190',\n" +
-                "     'jobs': [],\n" +
-                "     'last_name': 'Oved',\n" +
-                "     'phones': [],\n" +
-                "     'urls': []}]},\n" +
-                "  {'company': {'addresses': [{'city': 'NEW YORK',\n" +
-                "      'country_code': 'USA',\n" +
-                "      'line1': '1185 6TH AVE FL 10',\n" +
-                "      'postal_code': '10036',\n" +
-                "      'state_code': 'NY'}],\n" +
-                "    'emails': [],\n" +
-                "    'id': '2987b8f5-9d94-5858-b065-1fc62b315e80',\n" +
-                "    'match_score': 53,\n" +
-                "    'name': '31 WEST 34TH STREET LLC',\n" +
-                "    'phones': [],\n" +
-                "    'urls': []},\n" +
-                "   'contact_type': 'company',\n" +
-                "   'is_green_match': True,\n" +
-                "   'is_signatory': False,\n" +
-                "   'match_score': 53,\n" +
-                "   'persons': []}],\n" +
-                " 'owner_update_time': '2021-08-26',\n" +
-                " 'property_id': '2c1820dc-2a57-5532-8022-0a8840e32da7'}"
+        val jsonType = "{\n" +
+                "      \"name\": \"John Doe\",\n" +
+                "      \"age\": 30,\n" +
+                "      \"city\": \"New York\"\n" +
+                "}"
 
         Logx.j(jsonType)
         Logx.j(tag, jsonType)
